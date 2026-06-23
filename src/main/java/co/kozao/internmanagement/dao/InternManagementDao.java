7cb3639fc2b44352e50c46db3e6769a32004e79e
Ulrich <Yibaadev>
2026-06-23 17:20:30 +0100

    Add of InternMDao

--- src/main/java/co/kozao/internmanagement/dao/InternManagementDao.java
+++ src/main/java/co/kozao/internmanagement/dao/InternManagementDao.java
@@ -1,6 +1,21 @@
 package co.kozao.internmanagement.dao;
+import java.util.List;
+
+import co.kozao.internmanagement.model.*;
+
+public interface InternManagementDao {
+	
+	void create(Intern intern);
+	
+	Intern read(int id);
+	
+	List<Intern> readAll();
+	
+	void update(Intern intern);
+	
+	void delete(int id);
 
 
-public class InternManagementDao {
+	
 
 }
